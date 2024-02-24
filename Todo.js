6bede3bce8eeed6263e34df562884ba61699325c
eslammohamed12 <esomalk1@gmail.com>
2024-02-24 20:20:34 +0200

    adding main files

---
+++ Todo.js
@@ -0,0 +1,33 @@
+
+
+// let input = prompt("Enter a Command");
+// const todos = ['Studying', 'Playing'];
+
+// while (input !== 'Exit') {
+
+//     if (input === 'list') {
+
+//         for (let i = 0; i < todos.length; i++) {
+//             console.log(`${i}: ${todos[i]}`);
+//         } 
+    
+//     } else if (input === 'new') {
+//         const newTodo = prompt("Enter the new todo");
+//         todos.push(newTodo);
+        
+//             for (let k = 0; k < todos.length; k++){
+//                 console.log(`${k}; ${todos[k]}`);
+//             }
+        
+//     } else if (input === 'delete'){
+//         const deleteIndex = prompt("Enter an Index of todo to delete:");
+//         const deletedIndex = todos.splice(deleteIndex, 1);
+//         console.log("Fine, You have deleted a todo");
+        
+//     }
+
+//     input = prompt("Enter a Command");
+
+// }
+// console.log("Have a nice day.");
+
